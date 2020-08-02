e241f537e7e7a4e2aac527118b130fa7e125ef33
igoryy <igor.siviero1@gmail.com>
2020-08-02 11:34:14 -0300

    Corrigir porta

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const { quantidadePaginas } = require("./service/PaginaService");
 const CronJob = require('cron').CronJob
 const paginaService = require('./service/PaginaService');
-const job = new CronJob('0 */4 * * *' , () => { 
+const job = new CronJob('0 */1 * * *' , () => { 
     var strDate = new Date().getTime();;
 
     console.log(strDate);
     paginaService.quantidadePaginas();
 },null, true);
 
+//Importa as dependências que acabamos de instalar
+const express = require('express');
+const path = require('path');
 
+const app = express();
+
+// Serve os arquivos estáticos da pasta dist (gerada pelo ng build)
+app.use(express.static(__dirname + '/dist/NOME_DO_SEU_PROJETO'));
+
+
+
+// Inicia a aplicação pela porta configurada
+app.listen(process.env.PORT || 8080);
 
